cd452a84dfb2c3895b9ae3ce3661845bf81e0268
Quentin DUCASSE <quentin.ducasse@ensta-bretagne.org>
2021-02-08 16:59:21 +0100

    Changed type[length] name to type name[length]

--- pysdvu/formatter.py
+++ pysdvu/formatter.py
@@ -25,9 +25,9 @@ class Formatter():
         with open(filename, "w") as f:
             indent = 0
             nl = 0
-            fpe = 0
+            fpe = 0 # First process encountered
             for line in content:
-
+                # Process indentation
                 if line.startswith("process"):
                     fpe = 1
                     nl = 1
@@ -40,9 +40,6 @@ class Formatter():
                 elif line.startswith("guardCondition") and fpe == 1:
                     nl = 0
                     indent = 2
-                elif line.startswith("system async;") and fpe == 1:
-                    nl = 1
-                    indent = 0
                 elif fpe == 1:
                     indent = 3
                 f.write("\n"*nl + "  "*indent + line +"\n")
@@ -52,24 +49,22 @@ class Formatter():
         '''
         Process a file with propagated types.
         '''
-        temp_types = {}
-        glob_decl, temp_decl, proc = cls.preprocess(filename)
-        new_content = glob_decl
+        new_content = []
+        glob_decl, proc = cls.preprocess(filename)
 
-        # Process temporary types
-        for line in temp_decl:
-            split_line = line.split(" ")
-            type = split_line[1]
-            id = split_line.index("=")
-            name = split_line[id-1]
-            temp_types[name] = type
-        # Propagate type
-        for line in proc:
-            if line.startswith("t_"):
-                name = line.split(" ")[0]
-                new_content.append("temp " + temp_types[name] + " " + line)
+        for line in glob_decl:
+            if ('[' in line):
+                type = line.split('[')[0]
+                name = line.split(' ')[1]
+                length = line[line.index('[')+1:line.index(']')]
+                new_content.append(type + ' ' + name + '[' + length + '] = ' + line.split('=')[1])
             else:
                 new_content.append(line)
+
+        # Processes
+        for line in proc:
+            new_content.append(line)
+        print(new_content)
         return new_content
 
     @classmethod
@@ -84,18 +79,13 @@ class Formatter():
         content = [line for line in content if line] # Remove empty strings
         # Find indexes to cut the content in: glob decl, temp decl and process
         id = 0
-        temp_id = 0
         proc_id = 0
-        temp_passed = False
         for line in content:
-            if (line.startswith("temp") and not(temp_passed)):
-                temp_id = id
-                temp_passed =  True
             if (line.startswith("process")):
                 proc_id = id
                 break
             id += 1
-        return content[:temp_id], content[temp_id:proc_id], content[proc_id:]
+        return content[:proc_id], content[proc_id:]
 
 if __name__ == "__main__":
     import glob
